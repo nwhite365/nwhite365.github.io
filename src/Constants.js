b7f0f649a839cb1bff00f49f58a0922d3d3a054a
Nathan White <ntwhite@wisc.edu>
2024-03-07 08:52:40 -0600

    new publications

--- src/Constants.js
+++ src/Constants.js
@@ -4,6 +4,8 @@ import coframe from './static/publications/2022-hri-coframe.json';
 import understanding from './static/publications/2022-understanding.json';
 import expressions from './static/publications/2023-designing-expressions.json';
 import parentchild from './static/publications/2023-idc-parent-child.json';
+import makingdecisions from './static/publications/2024-hri-making-decisions.json';
+import enduserinterfaces from './static/publications/2024-hri-end-user-interfaces.json';
 
 // Ongoing Projects
 import coframejson from './static/ongoingprojects/coframe.json';
@@ -16,6 +18,6 @@ import sciencejson from './static/pastprojects/sciencelearning.json';
 import mathjson from './static/pastprojects/mathlearning.json';
 
 
-export const publicationJsons = [parentchild, expressions, understanding, coframe, robomath, designing];
+export const publicationJsons = [enduserinterfaces, makingdecisions, parentchild, expressions, understanding, coframe, robomath, designing];
 export const ongoingProjects = [coframejson, worldbeliefjson, planningjson, remotecollabjson];
 export const pastProjects = [sciencejson, mathjson];
