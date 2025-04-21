60b75cbaa5830b45c4e30737d683903c5045a156
Nathan White <ntwhite@pm.me>
2025-04-21 09:43:59 -0500

    Fixing icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nathantwhite.com',
-	integrations: [tailwind()],
+    site: 'https://nathantwhite.com',
+    integrations: [tailwind(), icon()],
 });
